7f868e96b717ad6e815d025afd7edaa330e738fd
Pramod Kumar <pramodkumar115@gmail.com>
2025-09-25 20:27:00 +0800

    Icons

--- lib/home.dart
+++ lib/home.dart
@@ -101,7 +101,7 @@ class _HomeState extends ConsumerState<Home> with TickerProviderStateMixin {
                 children: [
                   // HStack([
                   const GFAvatar(
-                    backgroundImage: AssetImage('assets/appicon.jpg'),
+                    backgroundImage: AssetImage('assets/AppIconNew.png'),
                     shape: GFAvatarShape.square,
                     backgroundColor: Color.fromRGBO(232, 237, 219, 0),
                     foregroundColor: Color.fromRGBO(232, 237, 219, 0),
